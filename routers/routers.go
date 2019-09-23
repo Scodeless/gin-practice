0a0317ae22216128f2729322194ad379c9a640cd
Scodeless <537464000@qq.com>
2019-09-23 17:28:15 +0800

    update bindrouter method

--- routers/routers.go
+++ routers/routers.go
@@ -9,7 +9,7 @@ import (
 
 func RouterInit() *gin.Engine {
 
-	router := gin.New()
+	router := gin.Default()
 	v1 := router.Group("/v1")
 	{
 		v1.POST("/auth/login", BindRouter(new(controller.AuthController), "Login"))
@@ -30,11 +30,10 @@ func BindRouter(ct controllerHelper.ControllerInterface, methodName string) gin.
 	if !ok {
 		panic("controller is not impolent controller interface")
 	}
-
+	method := reflectValue.MethodByName(methodName)
 	return func(ctx *gin.Context) {
 		execController.Init(execController, ctx)
 		execController.Prepare()
-		method := reflectValue.MethodByName(methodName)
 		method.Call(nil)
 		execController.Finish()
 	}
